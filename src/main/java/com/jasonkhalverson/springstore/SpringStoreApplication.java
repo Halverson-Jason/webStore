ccb1c6c292e8eea287160548355250f733cc0b9b
Jason Halverson <halv.jason@gmail.com>
2020-07-25 16:31:44 -0700

    added springboot

---
+++ src/main/java/com/jasonkhalverson/springstore/SpringStoreApplication.java
@@ -0,0 +1,13 @@
+package com.jasonkhalverson.springstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jasonkhalverson/springstore/SpringStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasonkhalverson.springstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
